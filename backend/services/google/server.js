6ce5ffca89a26acb45f20c990284d0e2da837a16
Petrişor,Rareş R.I <r.petrisor@student.fontys.nl>
2020-11-11 03:25:09 +0100

    Refactored code to match architecture and added support for both platforms in the backend

---
+++ backend/services/google/server.js
@@ -0,0 +1,192 @@
+let express = require('express')
+let queryString = require('query-string')
+let fetch = require('node-fetch')
+let bodyParser = require('body-parser')
+
+let {google} = require('googleapis');
+const { auth } = require('googleapis/build/src/apis/abusiveexperiencereport');
+let authClient = new google.auth.OAuth2(
+  '351958591751-3rlg46hcqh3soiasec3kj6n33m0m1h6r.apps.googleusercontent.com',
+  'B1K_CXLwGd5YCrRNDrJSgm8S',
+  'http://localhost:8889/callback'
+)
+
+let app = express()
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/auth',function(req,res)
+{
+  const url = authClient.generateAuthUrl(
+    {
+      access_type: 'offline',
+      scope:'https://www.googleapis.com/auth/youtube.readonly'
+    }
+  )
+  res.redirect(url);
+
+})
+
+app.get('/callback', async function(req,res)
+{
+  let code = req.query.code || null; 
+  const {tokens} = await authClient.getToken(code);
+
+  authClient.setCredentials(tokens);
+  let redirect_uri = process.env.redirect_uri || 'http://localhost:3000/';
+  //res.redirect(redirect_uri + '?access_token_google=' + tokens.access_token);
+
+  
+  res.send(tokens);
+})
+
+authClient.on('tokens', (tokens) => {
+  if (tokens.refresh_token) {
+    // store the refresh_token in my database!
+    authClient.setCredentials({
+      refresh_token : tokens.refresh_token
+    })
+    //console.log(tokens.refresh_token);
+  }
+  //console.log(tokens.access_token);
+});
+
+let nextPageToken = "";
+app.get("/playlist-data", function(req,res){
+   
+  
+  var service = google.youtube('v3');
+  service.playlists.list({
+    auth : authClient,
+    part : 'snippet,contentDetails',
+    mine : 'true',
+  }, function(err,response){
+    if(err){
+      console.log(err);
+      return;
+    }
+    
+
+    if(response.data.nextPageToken)
+    nextPageToken = response.data.nextPageToken
+
+    //console.log(nextPageToken);
+
+    let playlists = [];
+    response.data.items.map((playlist,index) => {
+      playlists[index] = {
+        id: playlist.id,
+        title: playlist.snippet.title,
+        thumbnail: playlist.snippet.thumbnails.maxres.url
+
+      }
+    })
+
+    res.send(playlists);
+
+  }
+  )
+
+})
+
+app.get("/playlist-data/next", function(req,res){
+   
+  
+  var service = google.youtube('v3');
+  service.playlists.list({
+    auth : authClient,
+    part : 'snippet,contentDetails',
+    mine : 'true',
+    pageToken: nextPageToken
+  }, function(err,response){
+    if(err){
+      console.log(err);
+      return;
+    }
+    
+
+    if(response.data.pageToken)
+    nextPageToken = response.data.nextPageToken
+
+    //console.log(nextPageToken);
+
+    let playlists = [];
+    response.data.items.map((playlist,index) => {
+      playlists[index] = {
+        id: playlist.id,
+        title: playlist.snippet.title,
+        thumbnail: playlist.snippet.thumbnails.maxres.url
+
+      }
+    })
+
+    res.send(playlists);
+
+  }
+  )
+
+})
+let nextPageTokenVideos = "";
+
+app.get("/playlist-info",function(req,res){
+
+
+  var service = google.youtube('v3');
+  service.playlistItems.list({
+    auth: authClient,
+    part:'snippet,id',
+    playlistId: req.query.playlistId
+  }, function(err,response){
+    if(err)
+    console.log(err);
+
+    if(response.data.nextPageToken)
+    nextPageTokenVideos = response.data.nextPageToken;
+
+    let videos = [];
+
+    response.data.items.map((video,index) =>{
+      videos[index] = {
+        title : video.snippet.title,
+        channelId: video.snippet.channelId
+
+      }
+    })
+    res.send(videos);
+  })
+
+})
+
+app.get("/playlist-info/next",function(req,res){
+
+
+  var service = google.youtube('v3');
+  service.playlistItems.list({
+    auth: authClient,
+    part:'snippet,id',
+    playlistId: req.query.playlistId,
+    pageToken: nextPageTokenVideos
+  }, function(err,response){
+    if(err)
+    console.log(err);
+
+    if(response.data.nextPageToken)
+    nextPageTokenVideos = response.data.nextPageToken;
+
+    let videos = [];
+
+    response.data.items.map((video,index) =>{
+      videos[index] = {
+        title : video.snippet.title,
+        channelId: video.snippet.channelId
+
+      }
+    })
+    res.send(videos);
+  })
+
+})
+
+
+let port = process.env.PORT || 8889
+console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
+app.listen(port)
---
+++ backend/services/spotify/server.js
@@ -0,0 +1,154 @@
+let express = require('express')
+let request = require('request')
+let querystring = require('query-string') 
+const bodyParser = require("body-parser")
+let SpotifyApi = require('spotify-web-api-node')
+
+
+let router = express.Router();
+let app = express()
+
+let SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || '70188bbbe3db4c088c59431261db0cd3';
+let SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || '5439baa2e8cd4c19aec16184f2e68f0c';
+let redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:8888/callback'
+
+let spotifyApi = new SpotifyApi();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/",router);
+
+
+router.get('/auth', function(req, res) {
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+router.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  }
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
+    res.send(access_token);
+    //res.redirect(uri + '?access_token_spotify=' + access_token)
+  })
+})
+
+router.get("/spotify-user-data", function(req,res){
+            
+  spotifyApi.getMe()
+  .then(function (data) {
+    console.log(data.body.display_name,data.body.spotify);
+    res.send(data);
+  })
+  .catch(function(error){
+    console.log(error);
+    res.send("failed");
+  })
+
+})
+
+router.get("/spotify-user-playlists",function(req,res){
+
+  spotifyApi.getUserPlaylists()
+  .then(function (data) {
+    let playlists = [];
+    data.body.items.map((item,index) => {
+      playlists[index] = {
+        id : item.id,
+        image : item.images[0].url,
+        name : item.name,
+        tracks : item.tracks.href,
+        trackCount : item.tracks.total
+      }
+    })
+    console.log(playlists);
+    res.send(playlists);
+  })
+  .catch(function(error){
+    console.log(error);
+    res.send("failed");
+  })
+
+})
+
+router.get("/spotify-search-tracks",function(req,res){
+  spotifyApi.getPlaylistTracks(req.body.playlistID)
+  .then(function (data){
+    
+    let songs = []
+    data.body.items.map((song,index) =>{
+
+      let artists = []
+      song.track.artists.map((artist,index) =>{
+        artists[index] = {
+          name : artist.name,
+          id: artist.id
+        }
+      })
+      songs[index] = {
+        artist : artists,
+        name : song.track.name,
+        url : song.track.external_urls.spotify
+
+      }
+      
+    })
+
+    res.send(songs);
+
+  })
+  .catch(function (error) {
+    res.send(error);
+  })
+})
+
+router.post("/set-spotify-token",function(req,res){
+
+  spotifyApi.setAccessToken(req.body.token);
+  res.send("success");
+
+})
+
+router.get("/search",function(req,res){
+  let trackName = req.query.trackName;
+  if(trackName.search(/official/i) > 0)
+    trackName = trackName.substring(0,trackName.search(/official/i)-1);
+
+  if(trackName.indexOf("(") >0)
+    trackName = trackName.substring(0,trackName.indexOf("(")-1);
+  console.log(trackName);
+
+  spotifyApi.searchTracks(trackName)
+  .then(function(data){
+    res.send(data.body);
+  }, function(err){
+    res.send(err);
+  })
+})
+
+let port = process.env.PORT || 8888
+console.log(`Listening on port ${port}. Go / to initiate authentication flow.`)
+app.listen(port)
